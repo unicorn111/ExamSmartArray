1a0a6d448f556f36179df299acb8a2afa8461029
unicorn111 <pryshlyak@ucu.edu.ua>
2018-01-14 16:37:32 +0200

    Update MapDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/MapDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/MapDecorator.java
@@ -2,16 +2,28 @@ package ua.edu.ucu.smartarr;
 
 import ua.edu.ucu.functions.MyFunction;
 
+import java.util.ArrayList;
+
 // Map every element to another object using MyFunction
 public class MapDecorator extends SmartArrayDecorator{
-
+    MyFunction func;
+    ArrayList<Object> arr;
     public MapDecorator(SmartArray smartArray, MyFunction func) {
         super(smartArray);
+        this.func = func;
+        this.arr = new ArrayList<>();
+        for (Object ob : smartArray.toArray()) {
+            ob = func.apply(ob);
+            arr.add(ob);
+        }
+        for (int i = 0; i< arr.size(); i++){
+            smartArray.toArray()[i] = arr.get(i);
+        }
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return smartArray.toArray();
     }
 
     @Override
@@ -21,6 +33,6 @@ public class MapDecorator extends SmartArrayDecorator{
 
     @Override
     public int size() {
-        return 0;
+        return smartArray.size();
     }
 }
